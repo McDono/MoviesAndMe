c9dfa93a798349d443e0ced8456351daa6cedd3c
Donovan <mcdonovan@outlook.fr>
2020-01-19 22:47:07 +0100

    inital commit

---
+++ components/filmItem.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+class FilmItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  };
+  }
+  render() {
+    return (
+      <View style={styles.main_container}>
+        <Text style={styles.title_text} >Titre du film</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  main_container : {
+    height: 190
+  },
+  title_text: {
+    
+  }
+})
+
+export default FilmItem;
---
+++ components/search.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, TextInput, Button, FlatList, Text } from 'react-native';
+import films from './../helpers/filmsData';
+import FilmItem from './filmItem';
+
+class Search extends Component {
+  constructor() {
+    return super();
+  }
+  render() {
+    return (
+      <View style={styles.mainContainer}>
+        <TextInput
+          style={styles.textInput }
+          placeholder="Titre du film"
+        />
+        <Button title="Rechercher" onPress={() => {}} />
+        <FlatList 
+          data={films}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({item}) => <FilmItem />}
+          />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    marginTop: 20
+  },
+  textInput: {
+    marginLeft: 5,
+    marginRight: 5,
+    height: 50,
+    borderColor: '#000000',
+    borderWidth: 1,
+    paddingLeft: 5
+  }
+})
+
+export default Search;
